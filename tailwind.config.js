9378753e0b66ec7068a6e0d2e8f738e47337b43d
saaddfk <127431428+saaddfk@users.noreply.github.com>
2025-05-31 20:30:20 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './resources/**/*.antlers.html',
+    './resources/**/*.blade.php',
+    './resources/**/*.vue',
+    './content/**/*.md'
+  ],
+  theme: {
+    
+     
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+    
+  ],
+}
